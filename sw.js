542d8cc0342dd52ff4d1ba46f0624eb468c51030
Praveen Kumar <85422629+Owlcode1@users.noreply.github.com>
2025-06-30 15:02:40 +0530

    yes

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js
+const CACHE_NAME = "presento-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "./assets/css/main.css",
+  "./assets/js/main.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
